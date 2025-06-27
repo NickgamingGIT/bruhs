09fb212675bc55538d7577c144da35dc36dfb6b6
NickgamingGIT <ndt005@outlook.com>
2025-06-27 15:47:12 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+import { createServer } from "http";
+import { Server } from "socket.io";
+import express from "express";
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+//app.use(express.static("public"));
+
+const users = {};
+
+io.on("connection", socket => {
+    socket.on("join", username => {
+        users[socket.id] = username;
+        io.emit("join", {username, id: socket.id});
+        io.emit("message", `${users[socket.id]} has joined`);
+    });
+    socket.on("message", message => {
+        io.emit("message", `${users[socket.id]}: ${message}`);
+    });
+    socket.on("disconnect", () => {
+        io.emit("message", `${users[socket.id]} has left`);
+    });
+});
+
+server.listen(3000, () => {
+    console.log("server started ig");
+});
